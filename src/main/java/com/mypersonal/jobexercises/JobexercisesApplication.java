959152a3f164e95c66ca596c5e20eb16c96434cc
Nathan Adcock <nathan.adcock43@gmail.com>
2021-08-21 13:38:47 -0500

    Feat: Configured Pom Dependencies and created Jobs Controller file

---
+++ src/main/java/com/mypersonal/jobexercises/JobexercisesApplication.java
@@ -0,0 +1,13 @@
+package com.mypersonal.jobexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobexercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobexercisesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/controllers/JobsController.java
@@ -0,0 +1,8 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController @CrossOrigin
+public class JobsController {
+}
